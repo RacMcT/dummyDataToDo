48dab776594905c71cf9585df333bf0c069499bd
RacMcT <raccmct@gmail.com>
2021-01-25 20:17:08 -0600

    initial code copied from zollege

---
+++ script.js
@@ -0,0 +1,47 @@
+// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+
+let arrayOfTodos = [
+  {
+  "userId": 14,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+  "userId": 20,
+  "id": 2,
+  "title": "delectus aut autem",
+  "completed": false
+}]
+
+const fetchTodos = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then( (response) => response.json())
+  .then( (json) => arrayOfTodos = json)
+}
+
+const logTodos = () => {
+  console.log(arrayOfTodos)
+}
+
+const populateTodos = () => {
+
+}
+
+const arrayOfTodos1 = [
+  {
+  "userId": 14,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+  "userId": 20,
+  "id": 2,
+  "title": "delectus aut autem",
+  "completed": false
+}
+]
+
+console.log(arrayOfTodos1[0].userId) // => 14
+console.log(arrayOfTodos1[1].userId) // => 20
