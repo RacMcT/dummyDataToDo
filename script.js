2834319c3df31cc983d6deb422ba1890229a555d
RacMcT <raccmct@gmail.com>
2021-01-27 20:13:34 -0600

    imput ol js to start

--- script.js
+++ script.js
@@ -16,17 +16,57 @@ let arrayOfTodos = [
 
 const fetchTodos = () => {
   fetch('https://jsonplaceholder.typicode.com/todos')
-  .then( (response) => response.json())
-  .then( (json) => arrayOfTodos = json)
-}
+  .then( (response) => {
+    console.log ('OG json:', json)
+    arrayOfTodos = json
+  })
+  }
+  
+  const logTodos = () =>{
+    console.log(arrayOfTodos)
+  }
+ 
+
 
 const logTodos = () => {
   console.log(arrayOfTodos)
 }
 
 const populateTodos = () => {
-
+  const firstObj = arrayOfTodos [0]
+  const userId = firstObj.userId
+  arrayOfTodos.userId
+  arrayOfTodos.forEach(todo =>{
+    if (!todo.completed) {
+      createIncompleteElement (todo)
+    } else {
+      createElement (todo)
+    }
+  })
 }
+
+const createIncompleteElement = (todo) => {
+  //then capture the ol item into a variable (getElementByID)
+
+  let ol = document.querySelector('ol')
+
+  let ol2 = document.getElementByTagName('ol')
+  //console.log('ol2', ol)
+  //console.log('ol', 'ol')
+
+  ol.style.height = '100px'
+  ol.style.width = '100px'
+  ol.style.backgroundColor = 'blue'
+
+  let li = document.querySelector('li')
+
+
+
+  //createElement to make a new ListeningStateChangedEventcreate TextNode inside the li using the title property.
+  //Now append the text to the new Element.
+  //then append the element to the ol element
+}
+
 
 const arrayOfTodos1 = [
   {
