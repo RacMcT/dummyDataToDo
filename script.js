59f2e8b34a8042eb0e2de6fb7a2a4fc74b376b50
RacMcT <raccmct@gmail.com>
2021-01-28 14:08:03 -0600

    working through display bugs

--- script.js
+++ script.js
@@ -12,18 +12,23 @@
   "id": 2,
   "title": "delectus aut autem",
   "completed": false
-}]
-*/
+}] */
+
 
 
 const fetchTodos = () => {
   fetch('https://jsonplaceholder.typicode.com/todos')
   .then( (response) => {
-    console.log ('OG json:', json)
+    //console.log ('OG response:', response)
+    return response.json()
+  })
+  
+  .then((json) =>{
+    console.log('OG json:', json)
     arrayOfTodos = json
   })
   }
-  
+
   const logTodos = () =>{
     console.log(arrayOfTodos)
   }
@@ -31,22 +36,26 @@ const fetchTodos = () => {
 //Making buttons work with JSON auto-populating
 
 const populateTodos = () => {
-  const firstObj = arrayOfTodos [0]
+  const firstObj = arrayOfTodos[0]
   const userId = firstObj.userId
   arrayOfTodos.userId
-  arrayOfTodos.forEach(todo =>{
-    if (!todo.completed) {
-      createIncompleteElement (todo)
+  arrayOfTodos.forEach(todo => {
+    if (!todo.completed){
+      createIncompleteElement(todo)
     } else {
-      createElement (todo)
+      createIncompleteElement(todo)
     }
   })
 }
 
+
+
 const createIncompleteElement = (todo) => {
   //then capture the ol item into a variable (getElementByID)
-  let ol = document.getElementByID('todo-list')
-
+  //let ol = document.querySelector('ol')
+  let ol2 = document.getElementByTagName('ol')
+//console.log('ol2:', ol2)
+  console.log('ol:', ol)
     ol.style.height = '100px'
     ol.style.width = '100px'
     ol.style.backgroundColor = 'purple'
